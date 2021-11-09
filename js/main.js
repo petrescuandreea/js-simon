fb9368e344059cf56d8893b9152676b522c99cd0
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-09 15:27:52 +0100

    step logici

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*
+
+TRACCIA: Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+*/
+
+// 1. creo una funzione che mi generi 5 numeri casuali
+// 2. invoco la funzione 
+// 3. stampo i numeri in html 
+// 4. faccio partire un timer di 30 secondi 
+    // chiedo all'utente per 5 volte di inserire i numeri visualizzati => for
+    // inserisco i numeri in un array 
+        // scorro l'array di numeri inseriti dall'utente 
+            // se sono inclusi nell'array iniziale
+                // stampo i numeri presenti 
+            // altrimenti 
+                // dico all'utente che non ha indovinato nessun numero 
